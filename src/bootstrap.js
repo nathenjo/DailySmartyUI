7a98911286383535809207e6a286af2e804bcd7b
nathenjo <nateselectronicrepair@gmail.com>
2021-10-15 14:41:27 -0400

    Added exact route for home route and implemented withRouter HOC

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,29 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import reducers from "./reducers";
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-import "./style/main.scss";
+import './style/main.scss';
 
 import Home from './components/home';
-import Results from "./components/results";
+
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path='/' component={Home}/>
-          <Route path='/results' component={Results}/>
+            <Route path='/'exact component={Home}/>
+            <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    </Provider>
+    , document.querySelector('.app-wrapper'));
 }
-
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
