8dcc919daed3e70df9d66a2b803078835173c390
nathenjo <nateselectronicrepair@gmail.com>
2021-10-16 09:44:42 -0400

    Setup API get request and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
+import { SET_RECENT_POSTS } from './types';
+
+import axios from 'axios';
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        //perform request
-        console.log('Hello');
+        axios.get('https://api.dailysmarty.com/posts')
+        .then(response => {
+            dispatch({
+                type: SET_RECENT_POSTS,
+                payload: response.data.posts
+            })
+        }).catch(error => {
+            console.log('Get request error: ', error);
+        })
     }
 }
