a7d6e959f9c7981627a84ab76c604628863b0e8f
nathenjo <nateselectronicrepair@gmail.com>
2021-10-19 12:09:33 -0400

    Mapped state to props in results posts component

--- src/components/results-posts.js
+++ src/components/results-posts.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Post from './post';
 
 class ResultsPosts extends Component {
+
+    renderPosts() {
+        const posts = this.props.post.map((post, index) => {
+            return (
+                <Post key={index} {...post}/>
+            )
+        })
+        return posts;
+    }
+
     render() {
         return (
             <div className='results-posts'>
                 <div className='results-posts-wrapper'>
                     <ul className='results-posts'>
-                        <li>One</li>
-                        <li>Two</li>
-                        <li>Three</li>
+                        {this.renderPosts()}
                     </ul>
                 </div>
             </div>
@@ -19,7 +28,7 @@ class ResultsPosts extends Component {
 
 function mapStateToProps(state) {
     return{
-        state
+        posts: state.posts.resultsPosts
     }
 }
 
